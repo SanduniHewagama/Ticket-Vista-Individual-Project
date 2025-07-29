03faa4989d2dc74670f83c23b551db2672f0e355
SanduniHewagama <hewagamasanduni2@gmail.com>
2025-07-29 23:15:33 +0530

    change login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,8 @@ const Navbar = () => {
   const { user } = useUser();
   const { openSignIn } = useClerk();
 
-
-
   return (
+
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
       <Link to="/" className="max-md:flex-1">
         <img src={assets.logo} alt="" className="w-36 h-auto" />
@@ -41,11 +40,10 @@ const Navbar = () => {
           ) 
         }
        
-=======
-        <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
-          Login
-        </button>
+      </div>
 
+      <MenuIcon className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
+    </div>
   );
 };
 
