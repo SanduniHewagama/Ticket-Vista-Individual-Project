3b420fb90bf7c939e517e0dedae04b1a200681f2
SanduniHewagama <hewagamasanduni2@gmail.com>
2025-09-02 11:45:23 +0530

    fix index.js code error

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -6,50 +6,58 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
 
 // Inngest Function to save user data to a database
+// ...existing code...
 const syncUserCreation = inngest.createFunction(
     { id: 'sync-user-from-clerk'},
     { event: 'clerk/user.created' },
     async ({ event })=>{
-        const { id, first_name, last_name, email_addresses, image_url} = event.data
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_addresses,
-            name: first_name + ' ' + last_name,
-            image: image_url
+        try {
+            const { id, first_name, last_name, email_addresses, image_url} = event.data
+            const userData = {
+                _id: id,
+                email: email_addresses[0].email_address,
+                name: first_name + ' ' + last_name,
+                image: image_url
+            }
+            await User.create(userData)
+        } catch (error) {
+            console.error("Error in syncUserCreation:", error)
         }
-        await User.create(userData)
     }
 )
 
-// Inngest Function to delete user data to a database
 const syncUserDeletion = inngest.createFunction(
     { id: 'delete-user-with-clerk'},
     { event: 'clerk/user.deleted' },
     async ({ event })=>{
-
-        const {id} = event.data
-        await User.findByIdAndDelete(id)       
+        try {
+            const {id} = event.data
+            await User.findByIdAndDelete(id)       
+        } catch (error) {
+            console.error("Error in syncUserDeletion:", error)
+        }
     }
 )
 
-
-// Inngest Function to update user data to a database
 const syncUserUpdation = inngest.createFunction(
     { id: 'sync-user-from-clerk-updation'},
     { event: 'clerk/user.updated' },
     async ({ event })=>{
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            image: image_url
+        try {
+            const {id, first_name, last_name, email_addresses, image_url} = event.data
+            const userData = {
+                _id: id,
+                email: email_addresses[0].email_address,
+                name: first_name + ' ' + last_name,
+                image: image_url
+            }
+            await User.findOneAndUpdate({ _id: id }, userData)
+        } catch (error) {
+            console.error("Error in syncUserUpdation:", error)
         }
-        await User.findOneAndUpdate(id, userData)
     }
 )
-
-
+// ...existing code...
 
 export const functions = [
     syncUserCreation,
