7aebcdcd24020f2d4995ad9f0b13f674470db874
SanduniHewagama <hewagamasanduni2@gmail.com>
2025-09-10 09:36:09 +0530

    booking controllers create

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import showRouter from "./routes/showRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 const app = express();
 const port = 8000;
@@ -18,9 +19,11 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-app.get("/", (req, res) => res.send("Server is Live!"));
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.get("/", (req, res) => res.send("Server is Live!"))
+app.use("/api/inngest", serve({ client: inngest, functions }))
 app.use('/api/show', showRouter)
+app.use('api/booking', bookingRouter)
+
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
